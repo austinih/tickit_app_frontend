d360b2deb01aa58792aaf54a18469f077b675a98
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-03-31 15:58:21 -0500

    api working

--- tickit/src/components/Main.jsx
+++ tickit/src/components/Main.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState, useContext } from "react";
+import axios from "axios";
+
+
 
 export default function Main() {
+
+const [venue, setVenue] = useState({})
+
+
+
+
+useEffect(() => {
+    const renderVenues = async () => {
+      const response = await axios.get(`http://localhost:8000/venues/1`);
+      setVenue(response.data);
+      console.log(response.data)
+    };
+    renderVenues()
+  }, [])
+
+
+
+
+
+
+
+
+
+
     return(
         <div>
+            <h2>{venue.name}</h2>
             <h3>Home Page</h3>
         </div>
     )
