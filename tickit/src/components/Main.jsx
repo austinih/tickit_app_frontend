199268c7c7f1ff71079e146caacdd05e27d906bd
Toni <windtronic89@gmail.com>
2023-04-03 11:55:19 -0600

    updates for home page

--- tickit/src/components/Main.jsx
+++ tickit/src/components/Main.jsx
@@ -1,31 +1,49 @@
 import { useEffect, useState, useContext } from "react";
 import axios from "axios";
+import '../styles/main.css'
 
 
 
 export default function Main() {
 
-const [venue, setVenue] = useState({})
+    const [events, setEvents] = useState([])
 
+    useEffect(() => {
+        const renderEvents = async () => {
+          const response = await axios.get(`http://localhost:8000/events`);
+          setEvents(response.data);
+          console.log(response.data)
+         
+        };
+        renderEvents()
+    }, [])
 
-
-
-
-
-
-
-
-
+    if (!events) {
+        return <h1> loading please wait </h1>
+        
+    } else {
 
 
 
 
 
     return(
-        <div className="main-container">
-
-            <h3>Home Page</h3>
-          
+          <div className="event-list">
+            
+            {events.map((event) => (
+                <div className="event-card" style={{backgroundImage:`url('${event.image_url}')`}}>
+                    <h2 className="event-title">{event.title}</h2> 
+                    <p className="event-artist">{event.artist}</p>
+                    <p className="event-genre">{event.genre}</p>
+                    <p className="event-date">{event.date}</p>
+                    
+                    
+                 
+                </div>
+                ))}
+            
         </div>
-    )
-}
+        )
+    }
+
+}   
