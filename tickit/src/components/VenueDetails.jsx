9967b422e8ff87ab921bbee1baec2ce161b01a5d
Austin <austinih46@gmail.com>
2023-04-03 13:03:34 -0500

    updated CSS theme

--- tickit/src/components/VenueDetails.jsx
+++ tickit/src/components/VenueDetails.jsx
@@ -1,3 +1,17 @@
+import { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import '../styles/venueDetails.css'
+
 export default function VenueDetails() {
-    return
+    
+    
+    
+    
+    
+    
+    return (
+        <div>
+            <h1>Upcoming Events at </h1> 
+        </div>
+    )
 }
